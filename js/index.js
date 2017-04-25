29c92a493a865252bc2bcdda25d914e23d803ace
RedHood111 <Golden7side@yandex.ru>
2017-04-25 15:09:16 +0300

    Замена слайдера, подгонка картинок, контактов, совмещение с адаптивкой

--- js/index.js
+++ js/index.js
@@ -1,88 +1,68 @@
-$(".choose").click(function () {
-	$(".choose").addClass("active");
-	$(".choose > .icon").addClass("active");
-	$(".pay").removeClass("active");
-	$(".wrap").removeClass("active");
-	$(".ship").removeClass("active");
-	$(".pay > .icon").removeClass("active");
-	$(".wrap > .icon").removeClass("active");
-	$(".ship > .icon").removeClass("active");
-	$("#line").addClass("one");
-	$("#line").removeClass("two");
-	$("#line").removeClass("three");
-	$("#line").removeClass("four");
-})
-
-$(".pay").click(function () {
-	$(".pay").addClass("active");
-	$(".pay > .icon").addClass("active");
-	$(".choose").removeClass("active");
-	$(".wrap").removeClass("active");
-	$(".ship").removeClass("active");
-	$(".choose > .icon").removeClass("active");
-	$(".wrap > .icon").removeClass("active");
-	$(".ship > .icon").removeClass("active");
-	$("#line").addClass("two");
-	$("#line").removeClass("one");
-	$("#line").removeClass("three");
-	$("#line").removeClass("four");
-})
-
-$(".wrap").click(function () {
-	$(".wrap").addClass("active");
-	$(".wrap > .icon").addClass("active");
-	$(".pay").removeClass("active");
-	$(".choose").removeClass("active");
-	$(".ship").removeClass("active");
-	$(".pay > .icon").removeClass("active");
-	$(".choose > .icon").removeClass("active");
-	$(".ship > .icon").removeClass("active");
-	$("#line").addClass("three");
-	$("#line").removeClass("two");
-	$("#line").removeClass("one");
-	$("#line").removeClass("four");
-})
-
-$(".ship").click(function () {
-	$(".ship").addClass("active");
-	$(".ship > .icon").addClass("active");
-	$(".pay").removeClass("active");
-	$(".wrap").removeClass("active");
-	$(".choose").removeClass("active");
-	$(".pay > .icon").removeClass("active");
-	$(".wrap > .icon").removeClass("active");
-	$(".choose > .icon").removeClass("active");
-	$("#line").addClass("four");
-	$("#line").removeClass("two");
-	$("#line").removeClass("three");
-	$("#line").removeClass("one");
-})
-
-
-$(".choose").click(function () {
-	$("#first").addClass("active");
-	$("#second").removeClass("active");
-	$("#third").removeClass("active");
-	$("#fourth").removeClass("active");
-})
-
-$(".pay").click(function () {
-	$("#first").removeClass("active");
-	$("#second").addClass("active");
-	$("#third").removeClass("active");
-	$("#fourth").removeClass("active");
-})
-
-$(".wrap").click(function () {
-	$("#first").removeClass("active");
-	$("#second").removeClass("active");
-	$("#third").addClass("active");
-	$("#fourth").removeClass("active");
-})
-
-$(".ship").click(function () {
-	$("#first").removeClass("active");
-	$("#second").removeClass("active");
-	$("#third").removeClass("active");
-	$("#fourth").addClass("active");
-})
+$(document).ready(function() {
+							   
+		var currentPosition = 0;
+		var slideWidth = 500;
+		var slides = $('.slide');
+		var numberOfSlides = slides.length;
+		var slideShowInterval;
+		var speed = 6000;
+ 
+		//Assign a timer, so it will run periodically
+		slideShowInterval = setInterval(changePosition, speed);
+		
+		slides.wrapAll('<div id="slidesHolder"></div>')
+		  
+		slides.css({ 'float' : 'left' });
+		
+		//set #slidesHolder width equal to the total width of all the slides
+		$('#slidesHolder').css('width', slideWidth * numberOfSlides);
+		
+		$('#slideshow')
+			.prepend('<span class="nav" id="leftNav">Move Left</span>')
+			.append('<span class="nav" id="rightNav">Move Right</span>');
+		
+		manageNav(currentPosition);
+		
+		//tell the buttons what to do when clicked
+		$('.nav').bind('click', function() {
+			
+			//determine new position
+			currentPosition = ($(this).attr('id')=='rightNav')
+			? currentPosition+1 : currentPosition-1;
+										
+			//hide/show controls
+			manageNav(currentPosition);
+			clearInterval(slideShowInterval);
+			slideShowInterval = setInterval(changePosition, speed);
+			moveSlide();
+		});
+		
+		function manageNav(position) {
+			//hide left arrow if position is first slide
+			if(position==0){ $('#leftNav').hide() }
+			else { $('#leftNav').show() }
+			//hide right arrow is slide position is last slide
+			if(position==numberOfSlides-1){ $('#rightNav').hide() }
+			else { $('#rightNav').show() }
+		}
+ 
+		
+		//changePosition: this is called when the slide is moved by the timer and NOT when the next or previous buttons are clicked
+		function changePosition() {
+			if(currentPosition == numberOfSlides - 1) {
+				currentPosition = 0;
+				manageNav(currentPosition);
+			} else {
+				currentPosition++;
+				manageNav(currentPosition);
+			}
+			moveSlide();
+		}
+		
+		
+		//moveSlide: this function moves the slide 
+		function moveSlide() {
+				$('#slidesHolder').animate({'marginLeft' : slideWidth*(-currentPosition)});
+		}
+ 
+	});
